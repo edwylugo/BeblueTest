9b1730c2f91d2b21ac909a74a3e5eba2c7c3ec39
edwylugo <edwy@sdvirtua.com.br>
2019-07-21 14:49:53 -0300

    First Commit: Create Project

---
+++ app/src/main/java/edwy/lugo/beblue/MainActivity.kt
@@ -0,0 +1,12 @@
+package edwy.lugo.beblue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
